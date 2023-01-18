9191806db28e61e937e586dcc1aeb6b7bb65793f
Rashmi <rashmi.rrp@gmail.com>
2023-01-18 16:16:43 +0530

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Docker Jenkin integration!!!");
+        System.out.println("Docker Jenkin integration!");
     }
 }
