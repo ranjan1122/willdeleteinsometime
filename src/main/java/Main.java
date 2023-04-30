97f5babd40aaecd576f77f79636868ed4d92eee2
Rashmi <rashmi.rrp@gmail.com>
2023-04-30 18:37:53 +0530

    planing to push code to other repo...https://github.com/ranjan1122/willdeleteinsometime.git

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Docker Jenkin integration!!!");
         System.out.println("Docker Jenkin integration!");
+        System.out.println("Hello All Good morning231!!");
     }
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     void go(){
-        System.out.println("Hello All Good morning!!");
+        System.out.println("Hello All Good morning321!!");
     }
 }
